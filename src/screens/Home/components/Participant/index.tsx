90871a931994b9178c4a97f0a9fac47e504f090e
Leandro <prorootinfo@gmail.com>
2022-12-27 10:15:58 -0300

    Iniciando projeto react-native

---
+++ src/screens/Home/components/Participant/index.tsx
@@ -0,0 +1,11 @@
+import {Text, View } from 'react-native';
+
+export function Participant(){
+    return(
+        <View>
+            <Text>
+                Leandro Sávio
+            </Text>
+        </View>
+    )
+}
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,36 @@
+import {Text, TextInput, View, TouchableOpacity } from 'react-native';
+import { styles } from './styles';
+
+export function Home() {
+  function handleParticipantAdd() {
+    console.log("Você clicou no botão de Adicionar!")
+  }
+  
+    return (
+    <View style={styles.container}>
+    <Text style={styles.eventName}>
+      Nome do evento
+      </Text>
+
+    <Text style={styles.eventDate}>
+      Segunda, 26 de Dezembro de 2022.
+     </Text>
+
+        <View style={styles.form}>
+
+     <TextInput
+     style={styles.input}
+     placeholder="Nome do participante"
+     placeholderTextColor="#6B6B6B"
+     />
+
+     <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+        <Text style={styles.buttonText}>
+            +
+        </Text>
+
+     </TouchableOpacity>
+     </View>
+    </View>
+  ); 
+}
